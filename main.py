dc4a219bf87d21f159892da23f6f079e3fc1e433
Artem336600 <99437550+Artem336600@users.noreply.github.com>
2025-02-23 18:50:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 from supabase import create_client
+from mistralai import Mistral
 
 # Данные для подключения к Supabase
 SUPABASE_URL = "https://rgyhaiaecqusymobdqdd.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJneWhhaWFlY3F1c3ltb2JkcWRkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODI0NjkyOCwiZXhwIjoyMDUzODIyOTI4fQ.oZe5DEPVuSCAzeKZxLInsF8iJWXBEGS9I9H6gGMBlmc"
-
+api_key = 'smKrnj6cMHni2QSNHZjIBInPlyErMHSu'
+model = "mistral-small-latest"
+client = Mistral(api_key=api_key)
 # Подключение к Supabase
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -33,6 +36,44 @@ def get_products_info():
             for product in products
         ]
         return jsonify({"products": product_info})
+    except Exception as e:
+        return jsonify({"error": f"Произошла ошибка: {str(e)}"}), 500
+
+
+@app.route('/make_prod', methods=['POST'])
+def make_dish():
+    try:
+        # Получаем данные из POST-запроса (ожидаем JSON с полем, например, "message")
+        data = request.get_json()
+        user_message = data.get('message')
+
+        # Проверяем, что сообщение не пустое
+        if not user_message:
+            return jsonify({"error": "Ошибка: Введите вопрос."}), 400
+
+        # Формируем системное сообщение
+        system_message = "По запросу пользователя ты должен выводить наборы еды по заданной теме"
+        
+        # Отправляем запрос к Mistral
+        chat_response = client.chat.complete(
+            model=model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_message
+                },
+                {
+                    "role": "user",
+                    "content": user_message
+                },
+            ]
+        )
+
+        # Возвращаем ответ в формате JSON
+        return jsonify({
+            "response": chat_response.choices[0].message.content
+        })
+
     except Exception as e:
         return jsonify({"error": f"Произошла ошибка: {str(e)}"}), 500
 
